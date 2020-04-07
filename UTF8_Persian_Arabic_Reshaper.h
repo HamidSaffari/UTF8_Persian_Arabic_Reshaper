f634b52e5dbdfcea755245d91a7b710a042f8199
Hamid Saffari <35362339+HamidSaffari@users.noreply.github.com>
2020-04-07 20:43:40 +0430

    Update UTF8_Persian_Arabic_Reshaper.h

--- UTF8_Persian_Arabic_Reshaper.h
+++ UTF8_Persian_Arabic_Reshaper.h
@@ -9,10 +9,22 @@
 #ifndef UTF8_PERSIAN_ARABIC_RESHAPER_H
 #define UTF8_PERSIAN_ARABIC_RESHAPER_H
 
-#if ARDUINO >= 100
-#include "Arduino.h"
+
+#if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad, FraunchPad and StellarPad specific
+    #include "Energia.h"
+#elif defined(RPI) // Raspberry Pi
+    #define RaspberryPi
+
+    // Include libraries for RPi:
+    #include <string.h> /* memcpy */
+    #include <stdlib.h> /* abs */
+    #include <wiringPi.h>
+#elif defined(SPARK)
+    #include "application.h"
 #else
-#include "WProgram.h"
+    #include "WProgram.h"
 #endif
 
 //----------------------------------------------------------------------------
